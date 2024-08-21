99151d015987e4fb285c6d3adec4a3ef7ff729b4
Liban Yusuf <lyus01306@gmail.com>
2024-08-21 09:31:42 +0100

    Changed link

--- src/components/Hero/HeroDescription/HeroDescription.jsx
+++ src/components/Hero/HeroDescription/HeroDescription.jsx
@@ -1,19 +1,23 @@
-
-import styles from './HeroDescription.module.css'
+import Link from 'next/link';
+import styles from './HeroDescription.module.css';
 
 const HeroDescription = () => {
     return (
         <div className={styles.heroDescription}>
-        <p className={styles.heroHeadline}>
-          Discover the <br />
-          perfect fireplace ...
-        </p>
-        <p className={styles.consultationParagraph}>
-          Book consultation: <span className={styles.numberSpan}>
-          <a href="http://localhost:3000/DesignBooking"><button>Contact Us</button></a></span>
-        </p>
-      </div>
+            <p className={styles.heroHeadline}>
+                Discover the <br />
+                perfect fireplace ...
+            </p>
+            <p className={styles.consultationParagraph}>
+                Book consultation: 
+                <span className={styles.numberSpan}>
+                    <Link href="/DesignBooking">
+                        <button>Contact Us</button>
+                    </Link>
+                </span>
+            </p>
+        </div>
     )
 }
 
-export default HeroDescription
+export default HeroDescription;
