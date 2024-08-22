f1c80a7b1b23db0884dbc9eb4c7090678025140f
Liban Yusuf <lyus01306@gmail.com>
2024-08-22 12:21:39 +0100

    Fixing Css

--- app/DesignBooking/page.js
+++ app/DesignBooking/page.js
@@ -6,7 +6,7 @@ import ContactForm from "@/src/components/ContactForm/Contact";
 export default function Booking() { 
     return (
         <>
-            <h1>Booking</h1>
+            <h1 className={styles.booking}>Design Booking</h1>
             <ContactForm/>
         </>
     );
