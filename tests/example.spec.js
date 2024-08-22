ed11446d7102f22e4a8eaae49a85757bf13caa99
Liban Yusuf <lyus01306@gmail.com>
2024-08-22 14:39:21 +0100

    Css for submit review

---
+++ tests/example.spec.js
@@ -0,0 +1,9 @@
+// @ts-check
+import {test,expect} from '@playwright/test'
+
+test('This should be running', async () => {
+//load the homepage
+//click button to go to the  booking page
+//fill out booking form
+//
+})
